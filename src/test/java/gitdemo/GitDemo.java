37e9a202302fe880c8b3b5ac2f0d07216702110c
Ashwani Verma <ashwaniv59@gmail.com>
2025-07-24 15:36:53 +0530

    added

--- src/test/java/gitdemo/GitDemo.java
+++ src/test/java/gitdemo/GitDemo.java
@@ -1,12 +1,10 @@
 package gitdemo;
 
+import java.util.List;
+
 public class GitDemo {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello...!");
-		System.out.println("Hi.....");
-		
-		
+		System.out.println("Hello");
 	}
-
 }
